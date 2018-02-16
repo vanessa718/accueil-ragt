82e5a961d1161d51ce082cebc49110cf6026ec01
simplon <dekorte.tsrit@gmail.com>
2018-02-16 15:02:49 +0100

    ruud - dots on the i

--- js/app.js
+++ js/app.js
@@ -33,11 +33,60 @@ $(document).ready(function() {
     });
 });
 
-// Sticky TOP button
-// =================
+
+
+
+// ####### Native browser smooth scrolling with jQuery              #######
+// ####### https://css-tricks.com/snippets/jquery/smooth-scrolling  #######
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+// ####### END SMOOTH SCROLLING #######
+
+
+
+
+// #######  Sticky BACK to TOP button  #######
+// https://cotswoldphoto.co.uk/bootstrap-float-to-top-button 
+// 
 $(document).ready(function(){
       // "Back to top" for english version, "Haut de page" for french version
-      $('body').append('<div id="toTop" class="btn btn-info"><i class="fa fa-arrow-up"></i>Haut de page</div>');
+      $('body').append('<div id="toTop" class="btn btn-back"><i class="fa fa-arrow-up"></i>Haut de page</div>');
       $(window).scroll(function () {
       if ($(this).scrollTop() != 0) {
         $('#toTop').fadeIn();
@@ -49,24 +98,15 @@ $(document).ready(function(){
         $("html, body").animate({ scrollTop: 0 }, 600);
         return false;
     });
-    // Snippet from Thomas [16 h 49]
-    // $(‘[href^=“#”]’).click(function() {
-    //      $(“html, body”).animate({ scrollTop: 0 }, “slow”);
-    //      return false;
-    //    }
-    //  });
 });
+//
+// #######  END BACK to TOP button  #######
 
 
-
-// ####### FUNCTION IMAGE RANDOM #######
-//  document.getElementById('imageAleatoire').style.backgroundImage = "url(img/background/image" + Math.round(Math.random()*11+1) +".jpg)"; 
-// ####### END FUNCTION IMAGE RANDOM #######
-
-
-//#######  STARTS change BACKGROUND with season #######/////
-//https://www.digitalocean.com/community/tutorials/how-to-use-the-switch-statement-in-javascript
-
+// #######  STARTS change BACKGROUND with season #######
+// 
+// https://www.digitalocean.com/community/tutorials/how-to-use-the-switch-statement-in-javascript
+//
 var month = new Date().getMonth();
 var season = 'spring';
 var months = month+2;
@@ -99,6 +139,9 @@ switch (month){
 }
 
 document.body.style.backgroundImage = "url(img/background/"+ season + "/img_" + Math.round(Math.random()*3+1) + ".jpg)";
+
+// 
+// ####### END FUNCTION SEASON BACKGROUND #######
 
  
 })();
